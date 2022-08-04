c9a4aaf1e09c3d4795286cd1fe9d1e6884cc4682
RonaldVilorio <ronvilorio@gmail.com>
2022-08-04 15:49:00 -0400

    moveble package out, particles in

--- src/lib/components/Jumbo.svelte
+++ src/lib/components/Jumbo.svelte
@@ -1,7 +1,47 @@
-<script></script>
+<script>
+  import Particles from 'svelte-particles';
+  import { loadFull } from 'tsparticles';
+
+  let particlesUrl = 'http://foo.bar/particles.json';
+
+  let particlesConfig = {
+    particles: {
+      color: {
+        value: '#000',
+      },
+      links: {
+        enable: true,
+        color: '#000',
+      },
+      move: {
+        enable: true,
+      },
+    },
+  };
+
+  let onParticlesLoaded = (event) => {
+    const particlesContainer = event.detail.particles;
+
+    // you can use particlesContainer to call all the Container class
+    // (from the core library) methods like play, pause, refresh, start, stop
+  };
+
+  let particlesInit = async (engine) => {
+    // you can use main to customize the tsParticles instance adding presets or custom shapes
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  };
+</script>
 
 <header>
-  <div
+  <Particles
+    id="tsparticles"
+    options={particlesConfig}
+    on:particlesLoaded={onParticlesLoaded}
+    {particlesInit}
+  />
+  <!-- <div
     class="p-12 text-center relative overflow-hidden bg-no-repeat bg-cover h-96 lg:h-screen"
     style="background-image: url('https://mdbootstrap.com/img/Photos/Inne/ostia.jpg')"
   >
@@ -27,5 +67,5 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </header>
